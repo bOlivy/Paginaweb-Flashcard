d0cc1db270ff445ffdde3f6eeb979f681a5ba6c9
bOlivy <00001117710518sp@al.educacao.sp.gov.br>
2024-11-21 19:42:01 -0300

    FLASHCARD ISABELLA 3C

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Artes',
+    'Qual é a principal característica do cubismo?',
+    'A representação de objetos em formas geométricas, mostrando múltiplas perspectivas simultaneamente.'
+)
+
+criaCartao(
+    'Filosofia',
+    'O que é o mito da caverna em Platão?',
+    'É uma alegoria que representa a ignorância humana e o processo de busca pelo conhecimento e pela verdade.'
+)
+
+criaCartao(
+    'Sociologia',
+    'O que é anomia, segundo Durkheim?',
+    'Anomia é a falta de normas ou regras claras em uma sociedade, levando à desordem e ao aumento de comportamentos desviantes.'
+)
+
+criaCartao(
+    'Literatura',
+    'Qual é o principal elemento inovador na estrutura narrativa de Memórias Póstumas de Brás Cubas, de Machado de Assis?',
+    ' O principal elemento inovador é a narração em primeira pessoa feita por um narrador defunto, que conta sua história a partir do além, rompendo com a linearidade temporal e utilizando metalinguagem e ironia.'
+) 
